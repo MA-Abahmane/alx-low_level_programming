d81e561f9f30cad6dcd4e081e5b0f5332f37fb24
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-16 17:02:19 +0100

    C3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/*Start:main
+
+/**
+*Start: main
 * 
 *print out the variable declared
 *
 *Return: 0
 */
+
 int main(void)
 {
 char *a = "\"Programming is like building a multilingual puzzle";
