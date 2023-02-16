708e036822b737934388b56852fca1d6d9f77b18
Mohamed Amine <ma.abahmane@gmail.com>
2023-02-16 08:29:45 -0800

    C4

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prinf the var'
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+  char *a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  printf("%s", a);
+  return (1);
+}
