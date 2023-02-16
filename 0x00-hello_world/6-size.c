8046e1f0eb407d7bae3fda585ea1e37c0c3c7e07
Mohamed Amine <ma.abahmane@gmail.com>
2023-02-16 08:59:13 -0800

    C3

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prinf the var'
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main() 
+{
+
+  char*win32 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s))";
+  
+  char*win64 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s))";
+
+  
+#ifdef _WIN32 // Includes both 32 bit and 64 bit
+	  #ifdef _WIN64
+	      printf("%s", win64);
+	  #else 
+	      printf("%s", win32);
+	  #endif
+#else
+	  printf("%s", win64);
+#endif
+	return 0;
+}
