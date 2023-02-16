90257529d46d7155611d5f178faef6d8380f3d12
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-16 16:49:16 +0100

    C3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
 
 int main(void) {
-  char *a = "with proper grammar, but the outcome is a piece of art,\n ";
+  char *a = "with proper grammar, but the outcome is a piece of art,";
   printf("%s", a);
   return 0;
 }
