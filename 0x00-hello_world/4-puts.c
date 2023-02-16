61c3264e2d514b9908537be6d070d7ec3339cafd
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-16 17:09:45 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
-*
-*Start:main
-*
-*print out the variable declared
-*
-*Return: 0
-*
-*/
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
