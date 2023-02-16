07dd01c377865061cb1a22a8d6c632f3f81022bf
Mohamed Amine <ma.abahmane@gmail.com>
2023-02-16 07:45:49 -0800

    C3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void) {
+  char *a = "\"Programming is like building a multilingual puzzle\n";
+  puts(a);
+  return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void) {
+  char *a = "with proper grammar, but the outcome is a piece of art,\n ";
+  printf("%s", a);
+  return 0;
+}
