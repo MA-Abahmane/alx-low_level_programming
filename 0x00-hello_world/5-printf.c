a4ff04262ec1760a0ec6120aa84c286c21348e64
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-16 17:12:40 +0100

    C3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'printf the var'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 char *a = "with proper grammar, but the outcome is a piece of art,";
