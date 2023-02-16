5c26c3a55894e41567f80695db9f6c5eb20e25ae
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-16 16:48:12 +0100

    C3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-  char *a = "\"Programming is like building a multilingual puzzle\n";
+  char *a = "\"Programming is like building a multilingual puzzle";
   puts(a);
   return 0;
 }
