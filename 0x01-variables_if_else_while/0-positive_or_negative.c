5ce2945179ce41a6c93dfb0bc5fb63f642e4dc1f
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-17 15:39:35 +0100

    C1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-printf("%s", "is positive\n");
+printf("%d %s", n "is positive\n");
 else if (n < 0)
-printf("%s", "is negative\n");
+printf("%d %s", n "is negative\n");
 else
-printf("%s", "is zero\n");
+printf("%d %s", n "is zero\n");
 return (0);
 }
