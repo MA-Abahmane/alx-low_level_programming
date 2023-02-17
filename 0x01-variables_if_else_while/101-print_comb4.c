c508684a626b8c8f00a27350935e33fd32c621cb
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-17 17:54:23 +0100

    C4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,22 +14,19 @@ int i, j, k, l, f;
 
 f = 44;
 k = 32;
-  
+
 for (i = 48; i < 57; i++)
 {
 for (j = i + 1; j < 58; j++)
 {
 for (l = j+1; l < 58; l++)
 {
-// nums 00
 putchar(i);
 putchar(j);
 putchar(l);
 if (i != 55 || j != 56 || l != 57)
 {
-// parenteses ','
 putchar(f);
-// space ' '
 putchar(k);
 }
 }
