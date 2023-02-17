dad425ca91b3cd750e5088c9b1e5c3c6db423fde
Mohamed Amine <ma.abahmane@gmail.com>
2023-02-17 03:48:30 -0800

    C1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'make random num and print its diecription'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+srand(time(0));
+
+int rnd_num = rand() % 10;
+
+if (rnd_num > 0)
+printf("%s", "is positive\n");
+else if (rnd_num < 0)
+printf("%s", "is negative\n");
+else
+printf("%s", "is zero\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Welcome to my 0x01-variables_if_else_while directory
