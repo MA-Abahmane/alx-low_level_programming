46920a2c30ad88d3032005681a5a161fb8a4e8bc
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-17 19:12:20 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
  * Return: Always 1 (Success)
  */
 
-int main()
+int main(void)
 {
 
 char *win32 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n";
