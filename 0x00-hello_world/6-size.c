e11c784a8fb1b8597982c5be818434c38abe8ff5
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-17 19:11:18 +0100

    C4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,18 +8,17 @@
  * Return: Always 1 (Success)
  */
 
-int main() 
+int main()
 {
 
-  char*win32 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n";
-  
-  char*win64 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n";
+char *win32 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n";
+char *win64 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n";
 
-  
+
 #ifdef _WIN32
-	  printf("%s", win32);
+printf("%s", win32);
 #else
-	  printf("%s", win64);
+printf("%s", win64);
 #endif
-	return 0;
+return (0);
 }
