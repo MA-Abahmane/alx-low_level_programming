e22acad6adc9d537feab842f7e1440fcee5d6e62
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-19 15:31:08 +0100

    C3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,16 +11,19 @@
 int main(void)
 {
 
-char *win32_1 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\n";
-char *win32_2 = "Size of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n";
-char *win64_1 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 8 byte(s)\n";
-char *win64_2 = "Size of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n";
-
+char *win32_1 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\n;
+char *win32_2 = "Size of a long int: 4 byte(s)\n";
+char *win32_3 = "Size of a long long int: 8 byte(s)\n";
+char *win32_4 = "Size of a float: 4 byte(s)\n";
+char *win64_1 = "Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\n;
+char *win64_2 = "Size of a long int: 8 byte(s)\n";
+char *win64_3 = "Size of a long long int: 8 byte(s)\n";
+char *win64_4 = "Size of a float: 4 byte(s)\n";
 
 #ifdef _WIN32
-printf("%s%s", win32_1, win32_2);
+printf("%s%s", win32_1, win32_2, win32_3, win32_4);
 #else
-printf("%s%s", win64_1, win64_2);
+printf("%s%s", win64_1, win64_2, win64_3, win64_4);
 #endif
 return (0);
 }
