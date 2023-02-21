aaa93e4319511ff28b9b33eb415a1548343b134d
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-21 17:07:49 +0100

    C2

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,15 +3,8 @@
 #include <stdlib.h>
 
 /**
- * main.h - Entry point
- *
- * Description: [A a function that computes the absolute value of an integer]
- *
- * Return: Always abs_val
- *
- * code by: MAAbahmane
+ * isalpha - check if its a letter
  */
-
 int _abs(int n)
 {
 int abs_val = abs(n);
