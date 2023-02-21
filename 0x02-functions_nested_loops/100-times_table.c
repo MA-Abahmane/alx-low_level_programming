f731e835fb4cdf171180444daafafd6a14581a2a
Mohamed Amine <ma.abahmane@gmail.com>
2023-02-21 13:59:00 -0800

    C4

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+//#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - print tame table of given num
+ * @num:  given num
+ * Return:  the table of numbers
+ */
+
+void print_times_table(int n)
+{
+int i, j;
+
+if (n <! 0 || n >! 15)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+printf("%d", j * i);
+if (j != 9)
+{
+if ((i * (j + 1)) < 10)
+{
+printf(",  ");
+}
+else
+{
+printf(", ");
+}
+}
+}
+putchar('\n');
+}
+}
+return;
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+//#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - print tame table of given num
+ * @num:  given num
+ * Return:  the table of numbers
+ */
+
+void main(void)
+{
+int i = 0;
+
+while (i < 1024)
+{
+if (i % 5 == 0 || i % 3 == 0)
+{
+i = i;
+}
+}
+printf("%d", i);
+
+return;
+}
