9c45afd47e8f07dff66fbd0587aec6b69fc81769
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-21 22:30:39 +0100

    C3

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-
 /**
  * add - add 2 given numbers
  * @a:  first given number
  * @b:  second given number
  * Return:  the sum
  */
+
 int add(int a, int b)
 {
 int sum = a + b;
