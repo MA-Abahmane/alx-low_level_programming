4c5af24395f8a8a906e3ee3ea7e48e9e5e50874e
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-21 23:36:09 +0100

    C4

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,13 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
- * print_times_table - print tame table of given num
- * @num:  given num
- * Return:  the table of numbers
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-void naturals(void)
+int main(void)
 {
-int i = 0;
 
-while (i < 1024)
-{
-if (i % 5 == 0 || i % 3 == 0)
-{
-i = i;
-}
-}
-printf("%d", i);
-
-return;
+	return (0);
 }
