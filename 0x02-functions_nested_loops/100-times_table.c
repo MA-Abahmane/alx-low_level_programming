db34cc407759aa96a0ffcb06d538c916cd34765f
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-21 23:04:38 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,16 +11,16 @@ void print_times_table(int n)
 {
 int i, j;
 
-if (n <! 0 || n >! 15)
+if (n <! 0 && n >! 15)
 {
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 printf("%d", j * i);
-if (j != 9)
+if (j != n)
 {
-if ((i * (j + 1)) < n)
+if ((i * (j + 1)) > 10)
 {
 printf(",  ");
 }
