e8c17c5c25a850876e9f61cf11000522a3595b62
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-23 23:14:43 +0100

    C4

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,41 +5,20 @@
  * @n : given number
  * Return: None
  */
-void print_number(int n)
+int main()
 {
-long m; /* power of 10 */
-int c; /* boolean check */
-long num; /* convert int to long */
+int n = -44;
+unsigned int num = n;
 
-num = n;
-
-if (num < 0)
+if (n < 0)
 {
-num *= -1;
-_putchar('-');
+num = -num;
+putchar('-');
 }
-
-m = 1;
-c = 1;
-while (c)
+if (0 < (num / 10))
 {
-if (num / (m * 10) > 0)
-m *= 10;
-else
-c = 0;
+printf("%d", num / 10);
 }
-
-while (num >= 0)
-{
-if (m == 1)
-{
-_putchar(num % 10 + '0');
-num = -1;
-}
-else
-{
-_putchar((num / m % 10) + '0');
-m /= 10;
-}
-}
+putchar((num % 10) + '0');
+return (0);
 }
