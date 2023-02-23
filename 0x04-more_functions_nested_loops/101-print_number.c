56169fa6d3ce87995dc9fae2bf65991dcb2d4054
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-23 22:57:14 +0100

    C4

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include "main.h"
+
+/**
+ * print_number - print the number given
+ * @n : given number
+ * Return: None
+ */
+void print_number(int n);
+{
+unsigned int num = n;
+
+if (n < 0)
+{
+num = -num;
+putchar('-');
+}
+if (0 < (num / 10))
+{
+printf("%d", num / 10);
+}
+putchar((num % 10) + '0');
+return;
+}
