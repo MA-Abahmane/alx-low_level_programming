be9df9151869749daba6263966a70a9e4f064e3c
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-23 23:30:50 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_triangle - print a triangle using '#'
  * @size: hight and withs of triangle
  * Return: None
  */
-
 void print_triangle(int size)
 {
 int a, b, c, i, j, h;
@@ -17,17 +15,17 @@ for (i = 0; i < size; i++)
 {
 for (j = 0; j < b-1; j++)
 {
-putchar(' ');
+_putchar(' ');
 }
 for (h = 0; h <= c; h++)
 {
-putchar('#');
+_putchar('#');
 }
-putchar('\n');
+_putchar('\n');
 b--;
 c++;
 
 }
 if (size <= 0)
-putchar('\n');
+_putchar('\n');
 }
