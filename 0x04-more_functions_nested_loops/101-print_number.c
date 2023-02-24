060d5abe86d96e3953553c142229b2acdb22ea4a
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-24 23:09:07 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,41 +3,19 @@
 /**
  * print_number - print the number given
  * @n : given number
- * Return: None
  */
 void print_number(int n)
 {
-unsigned int m; 
-int c; 
-unsigned int num;
+	unsigned int i = n;
 
-num = n;
-
-if (num < 0)
-{
-num *= -1;
-putchar('-');
-}
-m = 1;
-c = 1;
-while (c)
-{
-if (num / (m * 10) > 0)
-m *= 10;
-else
-c = 0;
-}
-while (num >= 0)
-{
-if (m == 1)
-{
-putchar(num % 10 + '0');
-num = -1;
-}
-else
-{
-putchar((num / m % 10) + '0');
-m /= 10;
-}
-}
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
