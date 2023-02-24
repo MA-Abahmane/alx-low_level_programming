02aed0eb452df7daff597ac74a5f8b2dca528d13
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-24 23:28:25 +0100

    C0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,5 +9,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
