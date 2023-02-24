f0de96ee861b500e6cb98c561a570ef2515b32c1
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-24 23:06:17 +0100

    C4

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_number - print the number given
@@ -8,41 +7,37 @@
  */
 void print_number(int n)
 {
-	long m; /* power of 10 */
-	int c; /* boolean check */
-	long num; /* convert int to long */
+long m; 
+int c; 
+long num;
 
-	num = n;
-	/* negatives */
-	if (num < 0)
-	{
-		num *= -1;
-		_putchar('-');
-	}
+num = n;
 
-	/* count up */
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-
-	/* count down */
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+if (num < 0)
+{
+num *= -1;
+_putchar('-');
+}
+m = 1;
+c = 1;
+while (c)
+{
+if (num / (m * 10) > 0)
+m *= 10;
+else
+c = 0;
+}
+while (num >= 0)
+{
+if (m == 1)
+{
+_putchar(num % 10 + '0');
+num = -1;
+}
+else
+{
+_putchar((num / m % 10) + '0');
+m /= 10;
+}
+}
 }
