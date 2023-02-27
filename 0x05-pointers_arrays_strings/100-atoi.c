9d6077891458d7715661c8192c676116356ca79d
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-27 23:22:10 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,26 +10,15 @@
 int _atoi(char *s)
 {
 int i, j, f, x, len, num;
-
-
 len = 0;
 num = 0;
-
 while (s[len] != '\0')
-{
-len++;
-}
-  
-i = 0;
-f = 0;
-j = 0;
-x = 0;
-
+  len++; 
+i = 0, f = 0, j = 0, x = 0;
 while (i < len && f == 0)
 {
 if (s[i] == '-')
 j++;
-
 if (s[i] >= 48 && s[i] <= 58)
 {
 num = s[i] - 48;
@@ -37,10 +26,8 @@ if (j % 2)
 {
 num = -num;
 }
-
 x = x * 10 + num;
 f = 1;
-
 if (s[i + 1] < 48 || s[i + 1] > 58)
 {
 break;
@@ -49,11 +36,7 @@ f = 0;
 }
 i++;
 }
-
 if (f == 0)
-{
-return (0);
-}
-
+  return (0);
 return (x);
 }
