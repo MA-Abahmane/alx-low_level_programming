e954e4428f4e8b10a73a7ab8b316dbc20b15c428
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-27 15:40:38 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,32 +9,28 @@
  */
 void reverse(char* begin, char* end)
 {
-    char temp;
-    while (begin < end) {
-        temp = *begin;
-        *begin++ = *end;
-        *end-- = temp;
-    }
+char temp;
+while (begin < end) {
+temp = *begin;
+*begin++ = *end;
+*end-- = temp;
+}
 }
 
 void rev_string(char* s)
 {
-    char* word_begin = s;
- 
+char* word_begin = s;
+char* temp = s;
 
-    char* temp = s;
- 
-   
- 
-    while (*temp) {
-        temp++;
-        if (*temp == '\0') {
-            reverse(word_begin, temp - 1);
-        }
-        else if (*temp == ' ') {
-            reverse(word_begin, temp - 1);
-            word_begin = temp + 1;
-        }
-    }
-    reverse( temp - 1, s);
+while (*temp) {
+temp++;
+if (*temp == '\0') {
+reverse(word_begin, temp - 1);
+}
+else if (*temp == ' ') {
+reverse(word_begin, temp - 1);
+word_begin = temp + 1;
+}
+}
+reverse( temp - 1, s);
 }
