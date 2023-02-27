7ad0e802deded14f6624478f80339da8a16732cd
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-27 15:56:20 +0100

    C1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,14 +12,14 @@ void rev_string(char *s)
 int i;
 char rev = s[0];
 int count = 0;
-  
+
 while (s[count] != '\0')
 count++;
 for (i = 0; i < count; i++)
 {
---count;
 rev = s[i];
-s[i] = s[count];
 s[count] = rev;
+s[i] = s[count];
+--count;
 }
 }
