81c8e9b912a4bf6add78e5db2d37cf02eb2ead6c
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-27 15:33:28 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,17 @@
  * @s: given string
  * Return: None
  */
+void rev_string(char *s);
+int main(void)
+{
+	char s[11] = "Holberton!";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
+
 
 void rev_string(char *s)
 {
