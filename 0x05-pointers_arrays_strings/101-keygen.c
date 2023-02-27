6745911018ba32a39778d578cec6536bb5b02668
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-02-27 17:31:25 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,31 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+/**
+ * main - make a random password
+ *
+ * Return: 0
+ */
 
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+int i, j, num = 0;
+int pass[1000];	
 
-	sum = 0;	
-
-	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
-	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
-	}
-
-	return (0);
+srand(time(NULL));
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+num += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - num) - '0' < 78)
+{
+j = 2772 - num - '0';
+num += j;
+putchar(j + '0');
+break;
 }
-
+}
+return (0);
+}
