b6cdc05ff04d61e34a4b14a884d18c9494d11295
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-01 12:56:47 +0100

    C1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * _strcat - concat 2 given strings and return the result
  * @dest: 1st given string
  * @src:  2end given string
- * Return : return str the result of concatination
+ * Return: return the result of concatination
  */
 char *_strcat(char *dest, char *src)
 {
