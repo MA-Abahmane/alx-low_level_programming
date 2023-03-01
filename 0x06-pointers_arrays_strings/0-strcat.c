83f57e124c88553120e668543c1b56ec3b829053
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-01 12:52:52 +0100

    C1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * *_strcat - concat 2 given strings and return the result
+ * _strcat - concat 2 given strings and return the result
  * @dest: 1st given string
  * @src:  2end given string
  * Return : return the result of concatination
