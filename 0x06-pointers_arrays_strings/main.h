929ed001643dd1a993f3c31421ac7a7a59c3a4e3
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-02 14:21:02 +0100

    C0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,3 +8,5 @@ char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
