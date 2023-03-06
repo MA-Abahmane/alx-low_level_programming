7ac89937fbe0553fde095be1c5e2d830e0f08840
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-06 20:53:40 +0100

    C2

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,14 +5,13 @@
 /**
  * _strstr - a function that locates a substring.
  * @haystack: Given string
- * @needle: given string of letters to search for its 
+ * @needle: given string of letters to search for its
  * first occurence
  * Return: return the first occurence
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-char* str = strstr(haystack, needle);
+char *str = strstr(haystack, needle);
 return (str);
 }
-
