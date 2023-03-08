0958599ce0271928bef9f6ed9abfe17758e23cfb
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-08 16:40:45 +0100

    C1

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,11 +11,17 @@
 int factorial(int n)
 {
 if (n < 0)
-  return (-1);
+{
+return (-1);
+}
 
 else if (n == 0)
-  return (1);
+{
+return (1);
+}
 
 else
-  return (n * factorial(n - 1));
+{
+return (n * factorial(n - 1));
+}
 }
