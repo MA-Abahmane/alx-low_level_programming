614560ececeeee3662e16a0fdf2da7f49048f62b
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-08 16:47:56 +0100

    C1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number.
+ * _sqrt_recursion - a function that returns the natural square root of n.
  * @n: base number
  * @base: base == n
  * Return: return the natural square root of n.
