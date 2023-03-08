1ba242d961464a69d1595c55711553fa6fe22c5c
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-08 16:53:35 +0100

    C1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,11 +3,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - a function that returns the natural square root of n.
- * @n: base number
- * @base: base == n
- * Return: return the natural square root of n.
- *
  * tester - a function used to test powers on the base number
  * @power: testing powers
  * Return: the correct natural squareroot.
@@ -24,6 +19,14 @@ return (-1);
 
 return (tester(power + 1, base));
 }
+
+
+/**
+ * _sqrt_recursion - a function that returns the natural square root of n.
+ * @n: base number
+ * @base: base == n
+ * Return: return the natural square root of n.
+ */
 
 int _sqrt_recursion(int n)
 {
