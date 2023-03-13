4c8467e32c48fc2f0f3fff9f242235d3611b4b51
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-13 16:04:51 +0100

    C1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include<string.h>
 #include "main.h"
-
 /**
  * str_concat - a function that concatenates two strings
  * @s1: 1st given string
@@ -10,7 +9,6 @@
  * Return: "NULL" if s1 or s2s size is 0 / else return the allocated
  * memory location for the concatinated string.
  */
-
 char *str_concat(char *s1, char *s2)
 {
 char *ar;
@@ -20,17 +18,14 @@ if (s1 == 0)
 {
 s1 = "";
 }
-
 if (s2 == 0)
 {
 s2 = "";
 }
-
 if (s1 == NULL && s2 == NULL)
 {
 return (NULL);
 }
-
 size1 = strlen(s1);
 size2 = strlen(s2);
 
@@ -43,7 +38,6 @@ while (i < size1)
 ar[i] = s1[i];
 i++;
 }
-
 j = 0;
 while (i < size1 + size2)
 {
@@ -52,6 +46,5 @@ j++;
 i++;
 }
 ar[i] = '\0';
-
 return (ar);
 }
