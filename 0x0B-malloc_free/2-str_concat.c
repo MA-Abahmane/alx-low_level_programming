5eab138c2503428f5e42430321e696920db8099a
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-13 16:03:43 +0100

    C1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,42 +16,40 @@ char *str_concat(char *s1, char *s2)
 char *ar;
 int i, j, size1, size2, full_size;
 
-
 if (s1 == 0)
 {
-  s1 = "";
+s1 = "";
 }
 
 if (s2 == 0)
 {
-  s2 = "";
+s2 = "";
 }
 
 if (s1 == NULL && s2 == NULL)
 {
-  return (NULL);
+return (NULL);
 }
 
 size1 = strlen(s1);
 size2 = strlen(s2);
 
 full_size = size1 + size2;
-
 ar = malloc(sizeof(char) * full_size + 1);
 
 i = 0;
 while (i < size1)
 {
-  ar[i] = s1[i];
-  i++;
+ar[i] = s1[i];
+i++;
 }
 
 j = 0;
 while (i < size1 + size2)
 {
-  ar[i] = s2[j];
-  j++;
-  i++;
+ar[i] = s2[j];
+j++;
+i++;
 }
 ar[i] = '\0';
 
