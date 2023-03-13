16cfdc575b434d66d921d8ef406c18fe91494cd3
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-13 15:59:25 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,26 +10,29 @@
  * Return: "NULL" if s1 or s2s size is 0 / else return the allocated
  * memory location for the concatinated string.
  */
+
 char *str_concat(char *s1, char *s2)
 {
 char *ar;
-int i, j;
+int i, j, l1, l2;
 
-if (s1 == NULL)
+if (s1 == 0)
 {
-s1 = "";
+  s1 = "";
 }
-if (s2 == NULL)
+
+if (s2 == 0)
 {
-s2 = "";
+  s2 = "";
 }
+
 if (s1 == NULL && s2 == NULL)
 {
-return (NULL);
+  return (NULL);
 }
 
-int size1 = strlen(s1)
-int size2 = strlen(s2);
+int size1 = strlen(s1), size2 = strlen(s2);
+
 int full_size = size1 + size2;
 
 ar = malloc(sizeof(char) * full_size + 1);
@@ -37,16 +40,18 @@ ar = malloc(sizeof(char) * full_size + 1);
 i = 0;
 while (i < size1)
 {
-ar[i] = s1[i];
-i++;
-}  
+  ar[i] = s1[i];
+  i++;
+}
+
 j = 0;
 while (i < size1 + size2)
 {
-ar[i] = s2[j];
-j++;
-i++;
+  ar[i] = s2[j];
+  j++;
+  i++;
 }
 ar[i] = '\0';
+
 return (ar);
 }
