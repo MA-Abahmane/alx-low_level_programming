ab678af00175c69e03b96ca22093590eeb9f7450
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-16 13:30:29 +0100

    C1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,18 +15,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+int i;
 char *ar = NULL;
 
 if (nmemb == 0 || size == 0)
-{
 return (NULL);
-}
 
 ar = malloc(nmemb * size);
 
 if (ar == NULL)
-{
 return (NULL);
+
+for (i = 0; i < (nmemb * size); i++)
+{
+ar[i] = 0;
 }
 
 return (ar);
