0da96e84cb3643b4c5bb205bbac29372b2111b74
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-16 16:07:36 +0100

    C2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
  * memo_cpy - a function that copys n bytes from memory srce 
  *to memory dest
@@ -11,8 +9,7 @@
  */
 char *memo_cpy(char *dest, char *srce, unsigned int n)
 {
-	unsigned int i = 0;
-  
+unsigned int i = 0;
 while (i < n)
 {
 dest[i] = srce[i];
@@ -20,8 +17,6 @@ dest[i] = srce[i];
 }
 return (dest);
 }
-
-
 /**
  * _realloc - a function that allocates memory for
  * an array using malloc.
@@ -32,41 +27,32 @@ return (dest);
  * Return: "NULL" if s1 or s2s size is 0 / else return the allocated
  * memory location for the concatinated string.
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 unsigned int n;
 void *ar;
 
 if (new_size == old_size)
-{
 return (ptr);
-}
-
 if (ptr == NULL)
 {
 ar = malloc(new_size);
 return (ar);
 }
-
 if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
 }
-
 if (new_size < old_size)
 n = new_size;
 else
-n = old_size;
-
+n = old_size;	
 ar = malloc(new_size);
-
 if (ar == NULL)
 return (NULL);
 
 ar = memo_cpy(ar, ptr, n);
 free(ptr);
-
 return (ar);
 }
