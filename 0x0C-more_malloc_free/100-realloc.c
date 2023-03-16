2985f2238921f88f31327068ed19de792be0587c
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-16 16:04:40 +0100

    C1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,59 +1,72 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _memcpy - Copy n bytes from memory area src to memory area dest
- * @dest: Memory area to copy to
- * @src: Memory area to copy from
- * @n: Amount to copy from memory area
- *
- * Return: Pointer to area
+ * memo_cpy - a function that copys n bytes from memory srce 
+ *to memory dest
+ * @dest: given empty memory area to copy to
+ * @srce: given memory to copy from
+ * @n: Amount of bytes to copy from a memory area
+ * Return: Pointer to the allocated memory area
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *memo_cpy(char *dest, char *srce, unsigned int n)
 {
-	unsigned int i;
-
-	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
+	unsigned int i = 0;
+  
+while (i < n)
+{
+dest[i] = srce[i];
+++i;
+}
+return (dest);
 }
 
+
 /**
- * _realloc - Reallocate a memory block using malloc
- * @ptr: Old memory block
- * @old_size: Size of of old memory block
- * @new_size: Size the new memory block should be
- *
- * Return: Pointer to new memory space, NULL if it fails
+ * _realloc - a function that allocates memory for
+ * an array using malloc.
+ * @ptr: ptr is a pointer to the memory previously
+ *allocated with a call to malloc: malloc(old_size)
+ * @old_size: is the size, in bytes, of the allocated space for ptr
+ * @new_size: is the new size, in bytes of the new memory block
+ * Return: "NULL" if s1 or s2s size is 0 / else return the allocated
+ * memory location for the concatinated string.
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *nptr;
-	unsigned int min;
+unsigned int n;
+void *ar;
 
-	if (ptr == NULL)
-	{
-		nptr = malloc(new_size);
-		return (nptr);
-	}
-	if (ptr != NULL && new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size < old_size)
-		min = new_size;
-	else
-		min = old_size;
-	nptr = malloc(new_size);
-	if (nptr == NULL)
-		return (NULL);
-	nptr = _memcpy(nptr, ptr, min);
-	free(ptr);
-	return (nptr);
+if (new_size == old_size)
+{
+return (ptr);
+}
+
+if (ptr == NULL)
+{
+ar = malloc(new_size);
+return (ar);
+}
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+
+if (new_size < old_size)
+n = new_size;
+else
+n = old_size;
+
+ar = malloc(new_size);
+
+if (ar == NULL)
+return (NULL);
+
+ar = memo_cpy(ar, ptr, n);
+free(ptr);
+
+return (ar);
 }
