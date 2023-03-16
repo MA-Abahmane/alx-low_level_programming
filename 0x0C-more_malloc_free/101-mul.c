ae0925629a73dbc3744a76eb481743a9d57f788e
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-16 15:02:56 +0100

    C2

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * num_finder - a program that find out if string
+ * contains only nums.
+ * @s: given string to ckeck
+ * Return: return the char if its a number
+ * else return -1
+ **/
+
+int num_finder(char *s)
+{
+int n, i, len;
+
+n = 0;
+i = 0;
+
+len = strlen(s);
+while (i < len)
+{
+if (s[i] < '0' || s[i] > '9')
+{
+return (NULL);
+}
+else
+n = n * 10 + (s[i] - '0');
+i++;
+}
+return (n);
+}
+
+/**
+ * main -a program that multiplies two positive numbers.
+ * @argv: Given array from function input parameters
+ * @argc: array size
+ * Return: Print the result of the multiplication of two given numbers
+ * if no errors are encountered
+ */
+
+int main(int argc, char *argv[])
+{
+unsigned int num1, num2, mul;
+
+if (argc > 3 || argc < 3)
+{
+puts("Error\n");
+exit(98);
+}
+
+num1 = num_finder(argv[1]);
+num2 = num_finder(argv[2]);
+
+if (num1 == NULL || num2 == NULL)
+{
+puts("Error\n");
+exit(98);
+}
+
+mul = num1 * num2;
+
+printf("%d\n", mul);
+return (0);
+}
