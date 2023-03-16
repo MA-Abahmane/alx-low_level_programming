5d547c8596345a6f68130d429a68813e793c3447
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-16 12:40:11 +0100

    C1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,13 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *ar = NULL;
 int i, j, size1, size2, full_size;
 
-if (s1 == 0)
+if (s1 == 0 || s2 == 0)
 {
-s1 = "";
-}
-if (s2 == 0)
-{
-s2 = "";
+return(NULL);
 }
 
 size1 = strlen(s1);
@@ -32,8 +28,11 @@ full_size = size1 + size2;
 
 ar = malloc(sizeof(char) * (full_size + 1));
 
-if (ar != NULL)
+if (ar == NULL)
 {
+return (NULL);
+}
+
 i = 0;
 while (i < size1)
 {
@@ -48,6 +47,7 @@ ar[i] = s2[j];
 j++;
 i++;
 }
-}
+ar[i] = '\0';
+
 return (ar);
 }
