1aa5af1ce7ea15bebd7bbc1f18b71a15bbc0c951
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-17 11:23:19 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,68 +1,72 @@
 #include <stdlib.h>
 #include <stdio.h>
-/**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(char *argv)
-{
-	int i;
+#include <string.h>
+//#include "main.h"
 
-	i = 0;
-	while (argv[i])
-	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
-}
 /**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(char *s)
-{
-	int i, result;
+ * num_finder - a program that find out if string
+ * contains only nums.
+ * @s: given string to ckeck
+ * Return: return the char if its a number
+ * else return -1
+ **/
 
-	i = result = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		i++;
-	}
-	return (result);
+int num_finder(char *s)
+{
+int n, i, len;
+
+n = 0;
+i = 0;
+
+len = strlen(s);
+while (i < len)
+{
+if (s[i] < '0' || s[i] > '9')
+{
+return (0);
 }
+else
+n = n * 10 + (s[i] - '0');
+i++;
+}
+return (n);
+}
+
+
 /**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
+ * main -a program that multiplies two positive numbers.
+ * @argv: Given array from function input parameters
+ * @argc: array size
+ * Return: Print the result of the multiplication of two given numbers
+ * if no errors are encountered
+ */
+
 int main(int argc, char *argv[])
 {
-	int i;
+int num1, num2, mul;
 
-	malloc();
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (_isdigit(argv[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-	return (0);
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+
+num1 = num_finder(argv[1]);
+num2 = num_finder(argv[2]);
+
+if (num_finder(argv[1]) == 0 || num_finder(argv[2]) == 0)
+{
+printf("Error\n");
+exit(98);
+}
+  
+if (num1 == 0 || num2 == 0)
+{
+printf("Error\n");
+exit(98);
+}
+
+mul = num1 * num2;
+printf("%d\n", mul);
+return (0);
 }
