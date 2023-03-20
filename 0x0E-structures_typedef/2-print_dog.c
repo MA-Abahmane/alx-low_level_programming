518887d28c9b93c44f53b25ed45dd84f8fbbb679
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-20 16:01:39 +0000

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,13 +15,13 @@ d->owner = "(nil)";
 
 if (d != NULL)
 {
-if (isnan(d->age))
+if (d->age)
 {
-printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 else
 {
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
 }
 }
 }
