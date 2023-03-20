1b49488b741e1c74e195beed95f586fefeeeba67
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-20 15:28:47 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - a structure contaning dog info
  * @name: dog name
@@ -11,7 +13,7 @@ typedef struct dog
 char *name;
 char *owner;
 float age;
-};
+} dog_t;
 
 
 /* :function prototypes: */
@@ -20,3 +22,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
