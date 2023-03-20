de2ab669b9e62c111978d699b5e51f43cd746fa2
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-20 13:51:26 +0000

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_H_
-#define _FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < (0) ? (-x) : (x))
-#define ABS(x) ((x) > (0) ? (x) : (x))
+#define ABS(X) (((X) < (0)) ? (-X) : (X))
 
 #endif
