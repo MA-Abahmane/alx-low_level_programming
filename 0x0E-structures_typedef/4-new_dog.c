3b1d6be238a5260c8487df3a926cc524f2bed33c
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-20 17:51:37 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,6 +7,7 @@
  * @name: dog name
  * @age: dog age
  * @owner: owners name
+ * Return: Pointer to the structure copy
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -36,6 +37,7 @@ return (NULL);
 ptr->name = name;
 ptr->age = age;
 ptr->owner = owner;
+
 
 return (ptr);
 }
