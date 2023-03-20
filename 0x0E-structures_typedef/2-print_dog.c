51b0f7d46032b79be66c562197d4d856babe9e65
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-20 16:11:05 +0000

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,13 @@
 
 void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
 if (!d->name)
 d->name = "(nil)";
 if (!d->owner)
 d->owner = "(nil)";
 
-if (d != NULL)
-{
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 }
