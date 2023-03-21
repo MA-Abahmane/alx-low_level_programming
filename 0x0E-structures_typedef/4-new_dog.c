41fe02fca3426df9c9870c9fa792fa7b41bae38f
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-21 10:24:33 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-
 /**
- * stringer - a function that creates a copy out of
- * a given string.
+ * stringer - a function that creates a copy of a string.
  * @s: given string to copy
  * Return: return the copy
  */
-
 char *stringer(char *s)
 {
 int i, len;
 char *str;
-
 len = strlen(s) + 1;
 str = malloc(sizeof(char) * len);
-
 if (str == NULL)
 return (NULL);
 
@@ -30,25 +24,20 @@ str[i] = s[i];
 str[i] = '\0';
 return (str);
 }
-
 /**
  * new_dog - a function that creates a new dog.
- * dogs.
  * @name: dog name
  * @age: dog age
  * @owner: owners name
  * Return: Pointer to the structure copy
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *ptr;
 ptr = malloc(sizeof(dog_t));
 
 if (ptr == NULL)
-{
 return (NULL);
-}
 
 ptr->name = stringer(name);
 if (!name)
@@ -64,8 +53,6 @@ free(ptr);
 free(name);
 return (NULL);
 }
-
 ptr->age = age;
-
 return (ptr);
 }
