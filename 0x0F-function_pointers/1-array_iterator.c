f069735a7a3e7d0249ed2c3bdb2e3475c90e8da7
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-22 13:03:01 +0000

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-
-	for (i = 0; i < size; i++)
+	int i;
+	i = 0;
+	while (i < size)
+	 {
 		action(array[i]);
+		i++;
+	 }
 }
