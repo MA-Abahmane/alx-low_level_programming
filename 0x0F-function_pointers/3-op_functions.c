9688d104ef8666d5b04c9e7a5f820e2cd1cb65ab
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-22 14:20:56 +0000

    C1

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,27 +1,53 @@
 /**
- *  - Function used to perform calculations using numbers.
+ * op_add - add up 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the resulte
  */
-
 int op_add(int a, int b)
 {
 return (a + b);
 }
 
+/**
+ * op_add - subtract 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the resulte
+ */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
 
+/**
+ * op_add - multiply 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the resulte
+ */
 int op_mul(int a, int b)
 {
 return (a * b);
 }
 
+/**
+ * op_add - devide 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the resulte
+ */
 int op_div(int a, int b)
 {
 return (a / b);
 }
 
+/**
+ * op_add - find the modulo of 2 numbers
+ * @a: num 1
+ * @b: num 2
+ * Return: the modulo
+ */
 int op_mod(int a, int b)
 {
 return (a % b);
