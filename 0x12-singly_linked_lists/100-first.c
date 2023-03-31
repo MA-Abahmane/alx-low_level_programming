09835f710c877aa42a69d729c72c1054a21bf08f
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-31 02:11:35 +0000

    C1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
 void startup_Fun(void) __attribute__ ((constructor));
+
 /**
  * startup_Fun - a function that executes before the main.
  *
@@ -9,6 +10,6 @@ void startup_Fun(void) __attribute__ ((constructor));
 
 void startup_Fun(void)
 {
-printf("You're beat! and yet, you must allow,\n")
+printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
