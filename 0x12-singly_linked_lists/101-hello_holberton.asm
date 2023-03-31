f15526262f7351d24fbd2b80449e507741b6d0bd
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-03-31 02:15:16 +0000

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,18 @@
-global start
+global _start
 
 section .text
-start:
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, message
-  mov rdx, 13
-  syscall
 
-  mov eax, 60
-  xor rdi, rdi
-  syscall
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
 
-message:
-  db "Hello, Holberton", 10
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, world!", 10
+  msglen: equ $ - msg
