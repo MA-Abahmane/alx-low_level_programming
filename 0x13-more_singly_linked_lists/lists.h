f85242b3dd2a8b266afde2fdec1c0ea65e83b000
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-03 18:57:17 +0000

    C1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_H_
+#define LISTS_H_
 
 /* Librarys */
 #include <stdio.h>
@@ -16,12 +16,13 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 
 /* Function prototypes */
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
