dc07a6d66c67c9b8ce44f82f60396404409f85f8
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-03 18:44:42 +0000

    C1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_H_
+#define LISTS_H_
 
 /* Librarys */
 #include <stdio.h>
@@ -24,6 +24,7 @@ struct list_s *next;
 
 
 /* Function prototypes */
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
