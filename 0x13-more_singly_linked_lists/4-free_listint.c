1784476865f94e3b0488e79f2394f4f40ac4c214
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-03 19:05:23 +0000

    C1

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,19 +4,17 @@
  * free_listint - a function that frees a listint_t list.
  * @head: given linked list.
  *
- * Return: None
  */
 
 void free_listint(listint_t *head)
 {
 listint_t *next;
 
-while (head)
+while (head != NULL)
 {
-next = head->next;
-free(head->n);
-free(head);
-head = next;
+next = head;
+head = head->next;
+free(next);
 }
 
 }
