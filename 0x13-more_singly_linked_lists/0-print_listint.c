036f77f2bc62872ff93475b2841bf0e1ee892977
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-03 18:30:27 +0000

    C1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,10 +2,10 @@
 
 /**
  * print_listint - a function that prints all the
- * elements of a listint_t list.
+ *        elements of a listint_t list.
  * @h: given linked list.
  *
- * Return: the number of nodes
+ * Return: the number of nodes.
  */
 
 size_t print_listint(const listint_t *h)
