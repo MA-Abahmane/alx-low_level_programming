14473c3f622cfeb82b0d691b7b97aec9fbbdd2a9
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-05 01:32:40 +0000

    C2

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+  * find_listint_loop - a function that finds the loop in a linked list.
+  * @head: given linked list head pointer.
+  *
+  * Return: The address of the node where the loop starts,
+  *     or NULL if there is no loop
+  */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+
+head++;
+return (0);
+}
