6c87e43b16debe7e3b53fd5c03a0db9ca3bf6c2d
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-08 00:15:16 +0000

    P1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_bit - a function that returns the
- *    value of a bit at a given index.
- * @n: given number to convert to binary.
+ * set_bit - a function that sets the value
+ *    of a bit to 1 at a given index.
+ * @n: given binary.
  * @index: is the index of the bit you want to get.
  *
  * Return: the value of the bit at index index or
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int len;
 
-len = sizeof(unsigned long int) *8;
+len = sizeof(unsigned long int) * 8;
 if (len <= index)
 return (-1);
 
