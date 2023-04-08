83655f85ecbba2b6a22a4962e55c68581950e778
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-08 00:49:02 +0000

    C1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,5 +11,15 @@
 void print_binary(unsigned long int n)
 {
 unsigned long int num = n;
-printf("%B", num);
+
+/* shift number until it becomes zero or one*/
+if (num > 1)
+print_binary(num >> 1);
+
+/* print the binary in num*/
+if (num & 1)
+putchar('1');
+else
+putchar('0');
+
 }
