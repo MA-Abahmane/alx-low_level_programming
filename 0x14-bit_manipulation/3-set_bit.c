6ec1d064d4259607aaf9bf35eec08f70f2fab04c
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-08 02:57:56 +0000

    C1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,12 +2,12 @@
 
 /**
  * set_bit - a function that sets the value
- *    of a bit to 1 at a given index.
+ *   of a bit to 1 at a given index.
  * @n: given binary.
  * @index: is the index of the bit you want to get.
  *
  * Return: the value of the bit at index index or
- *    -1 if an error occured
+ *   -1 if an error occured
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
