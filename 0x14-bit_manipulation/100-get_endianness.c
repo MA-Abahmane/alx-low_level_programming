5462c087d3501f3a8a778d3d250c5dd96d75f85f
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-08 02:56:20 +0000

    C1

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,13 +3,13 @@
 /**
  * get_endianness - Write a function that checks the endianness.
  *
- * Return: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, 1 if little endian.
  */
 
 int get_endianness(void)
 {
 int hex = 0x70121210;
-char *p = (char*) &hex;
+char *p = (char *) &hex;
 
 if (*p == 0x70)
 return (0);
