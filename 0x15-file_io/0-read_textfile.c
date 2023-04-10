f0baf943a335ff805b9f3ec2f021e61f2e55ff31
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-10 02:53:15 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,23 +3,23 @@
 /**
  * read_textfile - a function that reads a text file and
  *   prints it to the POSIX standard output.
- * @filename: Given filename to read/write
- * @letters: the number of letters to read/write
+ * @filename: Given filename to read/write.
+ * @letters: the number of letters to read/write.
  *
  * Return: returns the actual number of letters it
- *   could read and print
+ *   could read and print.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t fl, size, Bcount;
+ssize_t size, Bcount;
 char *buffer;
 
 if (filename == NULL)
 return (0);
   
   /* open file to read only mode */
-fl = open(filename, O_RDONLY);
+ssize_t fl = open(filename, O_RDONLY);
 if (fl == -1)
 return (0);
   
@@ -33,12 +33,11 @@ Bcount = read(fl, buffer, letters);
   
   /* count each char writen from the buffer */
 size = write(STDOUT_FILENO, buffer, Bcount);
-
 if (size != Bcount)
-  return (0);
+return (0);
 
 
-/* close file */
+  /* close file */
 close(fl);
   
 return (size);
