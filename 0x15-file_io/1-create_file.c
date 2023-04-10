b3b4b006807f107e48dc8f36803962174f228153
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-10 02:57:49 +0000

    C1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,9 +3,9 @@
 /**
  * create_file - a function that creates a file.
  * @filename: Given filename.
- * @text_content: what to write in the file
+ * @text_content: text to write in the file.
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
@@ -14,7 +14,7 @@ int mkF;
 
 if (filename == NULL)
 return (-1);
-  
+
   /* open new file to write mode, then set permission to "rw-------" */
 mkF = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (mkF < 0)
