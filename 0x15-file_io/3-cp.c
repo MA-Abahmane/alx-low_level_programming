fcabe9136ca4eae2889d4264062aa530cce56374
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-11 02:30:57 +0000

    C2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-int fileFrom, fileTo, bwrite = 0, bread = 1024, char buffer[1024];
+int fileFrom, fileTo, bwrite = 0, bread = 1024
+char buffer[1024];
 
 if (argc != 3)
 {
@@ -19,7 +20,6 @@ if (fileFrom == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 }
-
 fileTo = open(argv[2], (O_WRONLY | O_CREAT | O_TRUNC),
 (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 if (fileTo == -1)
