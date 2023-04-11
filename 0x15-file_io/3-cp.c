18d97ea527d4ff4e510c66da5caaeee83c4a1b36
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-11 02:24:12 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,17 +12,17 @@ char buffer[1024];
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n")
+exit(97);
 }
-  /* Open origin file to read only mode */
 fileFrom = open(argv[1], O_RDONLY);
 if (fileFrom == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-  /* Open destin file to write only mode, set fileTo permission to "rw-rw-r--" */
-fileTo = open(argv[2], (O_WRONLY | O_CREAT | O_TRUNC), 
+ /* Open destin file to write only mode, set fileTo permission to -rw-rw-r-- */
+fileTo = open(argv[2], (O_WRONLY | O_CREAT | O_TRUNC),
 (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 if (fileTo == -1)
 {
@@ -41,7 +41,6 @@ bwrite = write(fileTo, buffer, bread);
 if (bwrite < bread)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
-
 if (close(fileTo) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
