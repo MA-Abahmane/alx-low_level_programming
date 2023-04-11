36eea5d22f56afc56b3ed568df5f17332a994d4b
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-11 02:27:37 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,29 +12,27 @@ char buffer[1024];
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 }
 fileFrom = open(argv[1], O_RDONLY);
 if (fileFrom == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 }
+
 fileTo = open(argv[2], (O_WRONLY | O_CREAT | O_TRUNC),
 (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 if (fileTo == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
+
 while (bread == 1024)
 {
 bread = read(fileFrom, buffer, 1024);
 if (bread == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]). exit(98);
 }
 bwrite = write(fileTo, buffer, bread);
 if (bwrite < bread)
@@ -42,8 +40,7 @@ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 if (close(fileTo) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo), exit(100);
 }
 if (close(fileFrom) == -1)
 {
