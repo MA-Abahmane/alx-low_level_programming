74de85c8393bb742ae059f4d70adcd6cd3b5d6be
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-04-11 02:17:54 +0000

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- * main - a program that copies the content of a
- *    file to another file.
+ * main - a program that copies the content of a file to another file.
  * @argv: given main parameters array.
  * @argc: array size.
- *
  * Return: 0 on success, exit on failur.
  */
 int main(int argc, char *argv[])
 {
 int fileFrom, fileTo, bwrite = 0, bread = 1024;
 char buffer[1024];
-
 
 if (argc != 3)
 {
@@ -24,8 +21,7 @@ if (fileFrom == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-  /* Open destination file to write only mode */
-  /* set fileTo permission to "rw-rw-r--" (664 in octal notation) */
+  /* Open destination file to write only mode set fileTo permission to "rw-rw-r--" */
 fileTo = open(argv[2], (O_WRONLY | O_CREAT | O_TRUNC), (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
 if (fileTo == -1)
 {
@@ -49,19 +45,15 @@ if (bwrite < bread)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
-
 if (close(fileTo) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
 exit(100);
 }
-
 if (close(fileFrom) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFrom);
 exit(100);
 }
-
-
 return (0);
 }
