abe5d4f4c4dfb5b85ef8d7fb1dc66b0cba7c94e4
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-18 23:07:18 +0100

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,20 +1,4 @@
-#include <string.h>
-#include <stdio.h>
-#include <signal.h>
-
-/**
- * struct dlistint_s - doubly linked list
- * @argv: main arguments array
- * @argc: main arguments array length
- *
- * Description: doubly linked list node structure
- * 
- */
-
-int main(int __attribute__((__unused__))argc, char __attribute__((__unused__))*argv[])
+int main()
 {
-
-raise(SIGSEGV);
-
 return (0);
 }
