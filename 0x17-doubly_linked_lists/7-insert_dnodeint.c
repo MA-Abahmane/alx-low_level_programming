7d82b2fabfa13ff5fdfee9e07c226ae941b3c7b2
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-05-18 16:09:12 +0100

    C1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,25 +1,22 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - a function that inserts a new node at a
- *    given position.
- * @h: list to change
- * @idx: new node index
- * @n: data of new node
+ * insert_dnodeint_at_index - a function that adds a new node at given index.
+ * @h: list to change.
+ * @idx: new node index.
+ * @n: data of new node.
  *
- * Return: pointer to new node
+ * Return: pointer to new node.
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *node, *ptr = *h;
+dlistint_t *node, *ptr;
 unsigned int count = 0;
 
 /* creat new node */
 node = malloc(sizeof(dlistint_t));
 if (node == NULL)
-  return (NULL);
-
+return (NULL);
 node->n = n;
 /* if new node will be set a head of the list */
 if (idx == 0)
@@ -32,27 +29,21 @@ node->next = *h;
 *h = node;
 return (node);
 }
-
 /* if: the head node in it larger than our node */
+*ptr = *h;
 while (count < (idx - 1) && ptr != NULL)
 {
-
 ptr = ptr->next;
 count++;
 }
-
 if (ptr)
 {
 if (ptr->next)
-{
 ptr->next->prev = node;
-}
 node->next = ptr->next;
 node->prev = ptr;
 ptr->next = node;
-
-return(node);
+return (node);
 }
-
 return (NULL);
 }
