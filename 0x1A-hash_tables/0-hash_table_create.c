674fb40ab572312237110824a936200ce5ad5eae
Mohamed Amine <ma.abahmane@gmail.com>
2023-06-08 12:55:27 +0100

    C1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,2 +1,34 @@
+#include "hash_tables.h"
 
-int main
+
+/**
+ * hash_table_create - a function that creates a hash table.
+ * @size: size is the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+int i = 0;
+hash_table_t *hashT;
+
+/* setting the hash table */
+hashT = malloc(sizeof(hash_table_t));
+if (hashT == NULL)
+  return (NULL);
+
+/* setting the tables nodes */
+hashT->size = size;
+hashT->array = malloc(sizeof(hash_node_t *) * size);
+if (hashT->array == NULL)
+  return (NULL);
+
+/* setting all table nodes to null */
+for (; i < size; i++)
+{
+hashT->array[i] = NULL;
+}
+
+  
+return (hashT);
+}
---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_djb2 - a hash function implementing the djb2 algorithm.
+ * @str: given string to conver to hash value
+ *
+ * Return: a pointer to the newly created hash table
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+int c;
+unsigned long int Hsh = 5381;
+
+/* calculates a hash value for a given */
+/* string using the djb2 algorithm */
+while ((c = *str++))
+{
+Hsh = ((Hsh << 5 ) + Hsh) + c;
+}
+
+return (Hsh);
+}
