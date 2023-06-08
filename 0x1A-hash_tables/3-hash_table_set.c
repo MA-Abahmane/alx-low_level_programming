8faa06b0d1babdcadef3d432603d5ae1f05c7d2c
Your Name <you@example.com>
2023-06-08 22:38:44 +0000

    C1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,36 +1,28 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set - a hash function implementing the djb2 algorithm.
  * @ht: ht is the hash table you want to add
  * @key: key is the key
  * @value: value is the value associated with the key
- *
  * Return: the index at which the key/value pair
- *    should be stored in the array of the hash table
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 unsigned long int idx;
 hash_node_t *newNode, *currnt;
 
-
 if (key == NULL || ht == NULL || value == NULL)
 return (0);
-
 /* get the index of the key */
 idx = key_index((unsigned char *)key, ht->size);
-
-/* allocating the new node */
 newNode = malloc(sizeof(hash_node_t));
 if (newNode == NULL)
 return (0);
-
 /* check all sub value keys for any match */
 currnt = ht->array[idx];
 while (currnt != NULL)
 {
-/* if match found; undate key value */
+/* Look for a matching key. no match; set the node as the head node */
 if (strcmp(currnt->key, key) == 0)
 {
 free(currnt->value);
@@ -42,8 +34,6 @@ return (1);
 }
 currnt = currnt->next;
 }
-
-/* no match; set the node as the head node */
 newNode->key = strdup(key);
 if (newNode->key == NULL)
 {
