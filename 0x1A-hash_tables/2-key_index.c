fe3291fcf9049c2b7f480754403250685d415475
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-06-08 16:50:21 +0100

    C1

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,7 +6,7 @@
  * @key: key is the key
  * @size: size is the size of the array of the hash table
  *
- * Return: the index at which the key/value pair 
+ * Return: the index at which the key/value pair
  *    should be stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
