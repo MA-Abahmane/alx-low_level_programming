815fec4d5703d6a0fea0586304f92a07aa615f44
Your Name <you@example.com>
2023-06-09 11:10:03 +0000

    C1

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function that prints a hash table.
+ * @ht: is the hash table
+ * Return: None
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+int f;
+hash_node_t *node;
+unsigned long int i;
+
+
+if (ht == NULL)
+return;
+
+i = 0;
+f = 0;
+printf("{");
+/* loop throut the hash table */
+while (i < ht->size)
+{
+node = ht->array[i];
+/* get the node values if its not NULL */
+while (node != NULL)
+{
+if (f == 1)
+printf(", ");
+
+printf("'%s': '%s'", node->key, node->value);
+node = node->next;
+f = 1;
+}
+  
+++i;
+}
+
+printf("}\n");
+}
