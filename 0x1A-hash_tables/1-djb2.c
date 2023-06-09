517274ed36aa3cb78f8e6897e901a92803eead62
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-06-09 15:52:29 +0100

    C1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,10 +9,11 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
 	int c;
+	unsigned long int hash = 5381;
 
-	hash = 5381;
+
+
 	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
