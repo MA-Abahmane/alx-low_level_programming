1db52c28fd2da2714a87ab3d2fb9aa90ba6f4a33
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-11 18:53:28 +0100

    Sorting

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,13 +7,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 /* Tools */
 int SFunc(int *array, int start, int end, int value);
 void print_array(int *array, int start, int end);
+
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+int jump_search(int *array, size_t size, int value);
 
 #endif
