9b2a61dd7aa1ccf2fe65e5a84635a2bf935ce9c5
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-12 20:19:59 +0100

    Searching

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,22 +21,22 @@ int jump_search(int *array, size_t size, int value)
 	/** Jump search algorithm **/
 	gap = floor(sqrt(size));
 
-    i = 0;
-    jump = 0;
+	i = 0;
+	jump = 0;
 	while (jump < (int)size && array[jump] < value)
-    {
-        printf("Value checked array[%d] = [%d]\n", jump, array[jump]);
-        i = jump;
-        jump += gap;
-    }
+	{
+		printf("Value checked array[%d] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += gap;
+	}
 
-    printf("Value found between indexes [%d] and [%d]\n", i, jump);
+	printf("Value found between indexes [%d] and [%d]\n", i, jump);
 
 	/* If out of range */
-    if (jump > (int)size - 1)
-        jump = size - 1;
+	if (jump > (int)size - 1)
+		jump = size - 1;
 
-    for (; i < jump && array[i] < value; i++)
+	for (; i < jump && array[i] < value; i++)
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
 	printf("Value checked array[%d] = [%d]\n", i, array[i]);
