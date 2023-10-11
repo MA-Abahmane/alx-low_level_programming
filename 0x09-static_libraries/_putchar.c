92695e5f7822daad62faadca990c8a3c7d198eb2
MAAbahmane <122897375+MA-Abahmane@users.noreply.github.com>
2023-10-11 23:25:36 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
+#include <unistd.h>
 
-int _putchar(char c){
-  (void)c
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
